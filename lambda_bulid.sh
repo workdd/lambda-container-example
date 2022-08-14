8fe7fc3c79d675955a8e10542659abd848dc8fa1
최재강 <chl8273@naver.com>
2022-08-14 21:11:08 +0900

    change image name

--- lambda_bulid.sh
+++ lambda_bulid.sh
@@ -1,13 +1,13 @@
 sudo service docker restart
 
-docker build -t jg-lambda-image . --no-cache
+docker build -t lambda-container-example . --no-cache
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
-docker tag jg-lambda-image $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/jg-lambda-image
+docker tag jg-lambda-image $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/lambda-container-example
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
 
-docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/jg-lambda-image
+docker push $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/lambda-container-example
 
 docker rmi -f $(docker images -q)

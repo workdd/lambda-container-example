69f01af8021b4e64692ccc27c62808ca21ac734f
최재강 <chl8273@naver.com>
2022-08-14 21:13:55 +0900

    change docker name

--- lambda_bulid.sh
+++ lambda_bulid.sh
@@ -4,7 +4,7 @@ docker build -t lambda-container-example . --no-cache
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
-docker tag jg-lambda-image $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/lambda-container-example
+docker tag lambda-container-example $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/lambda-container-example
 
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com
 
